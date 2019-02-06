1e763aea32651813df43f927d891297f77b97d1c
mitsuru793 <mitsuru793@gmail.com>
2019-02-07 00:35:28 +0900

    add test to cache file

--- tests/unit/SampleTest.php
+++ tests/unit/SampleTest.php
@@ -22,4 +22,17 @@ final class SampleTest extends \Codeception\Test\Unit
         var_dump('from var_dump');
         codecept_debug('from codecept_debug');
     }
+
+    public function testMakeDir()
+    {
+        $root = codecept_root_dir();
+        $dir = "$root/cache/sub/";
+        mkdir($dir, 0777, true);
+
+        $file = "$dir/file1";
+        file_put_contents($file, 'hello');
+
+        $cache = file_get_contents($file);
+        $this->assertEquals('hello', $cache);
+    }
 }
